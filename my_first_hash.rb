b2de7ceb7bad5112794c6611ce96db95371185ef
Shaina C <scailles23@gmail.com>
2019-12-29 02:08:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,6 +17,8 @@ def retrieval
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 puts shipping_manifest[3]
 end
+end
+
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
