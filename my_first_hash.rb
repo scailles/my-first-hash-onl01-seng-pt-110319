62e2d61de995fa7d46fcf6100bf477ca70452b93
Shaina C <scailles23@gmail.com>
2019-12-29 02:15:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,11 +26,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest["muskets"]=>2
+shipping_manifest["muskets"]=2
 end
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest["gun powder"]=>4
+shipping_manifest["gun powder"]=4
 end
   # return the shipping_manifest hash below
 return shipping_manifest
